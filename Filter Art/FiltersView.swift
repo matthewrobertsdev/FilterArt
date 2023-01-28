3203a9bc371dc4efc5d664e5b0b369cd0e57da4c
Matt <31932179+matthewrobertsdev@users.noreply.github.com>
2023-01-28 00:51:08 -0500

    Added search to saved filters

--- Filter Art/FiltersView.swift
+++ Filter Art/FiltersView.swift
@@ -40,6 +40,7 @@ struct FiltersView: View {
 	@State var showingRenameAlert = false
 	@State var filterToDelete: Filter? = nil
 	@State var filterToRename: Filter? = nil
+	@State var searchString = ""
     var body: some View {
 		#if os(macOS)
 		VStack(alignment: .center, content: {
@@ -64,64 +65,7 @@ struct FiltersView: View {
 					showing = false
 				}*/
 			} else if filterType == FilterType.saved.rawValue {
-				List(selection: $selectedSavedFilter) {
-					ForEach(savedFilters, id: \.self) { filter in
-						VStack(alignment: .center) {
-							HStack {
-								Spacer()
-								if isEditing {
-									VStack {
-										Button(role: .destructive) {
-											filterToDelete = filter
-											showingDeleteDialog = true
-										} label: {
-											Label("Delete", systemImage: "trash.fill").labelStyle(.titleOnly)
-										}.buttonStyle(.borderless).tint(Color.red)
-										Button {
-											filterToRename = filter
-											showingRenameAlert = true
-										} label: {
-											Label("Rename", systemImage: "pencil").labelStyle(.titleOnly)
-										}.buttonStyle(.borderless).tint(Color.indigo)
-									}.transition(.move(edge: .leading))
-
-									Spacer()
-								}
-								getFilteredImage(filter: filter).frame(width: 250, height: 175).transition(.move(edge: .leading))
-								Spacer()
-							}
-							HStack {
-								Spacer()
-								Text(filter.name ?? "Saved Filter")
-								Spacer()
-							}
-						}
-					}
-				}.listStyle(.sidebar)/*.onChange(of: selectedSavedFilter) { newValue in
-					asignSavedFilterComponentsToAppStorage()
-					showing = false
-				}*/.confirmationDialog(Text("Are you sure you want to delete this filter?"), isPresented: $showingDeleteDialog) {
-					Button(role: .destructive) {
-							  DispatchQueue.main.async {
-								  if let filter = selectedSavedFilter {
-									  managedObjectContext.delete(filter)
-									  selectedSavedFilter = nil
-									  do {
-										  try managedObjectContext.save()
-									  } catch {
-										  
-									  }
-								  }
-							  }
-						  } label: {
-							  Text("Delete Filter")
-						  }
-
-					  }.alert("Rename Your Filter", isPresented: $showingRenameAlert, actions: {
-						  RenameAlert(selectedSavedFilter: $selectedSavedFilter).environment(\.managedObjectContext, managedObjectContext)
-		  }, message: {
-			  Text("Eenter a new name for your filter:")
-		  })
+				SavedFiltersView(showing: $showing, searchString: searchString)
 			} else {
 				EmptyView()
 			}
@@ -135,41 +79,12 @@ struct FiltersView: View {
 				} label: {
 					Text("Filters Picker")
 				}.pickerStyle(.segmented).labelsHidden()
-				TextField("", text: .constant(""), prompt: Text("Search \(filterType.lowercased())..."))
+				TextField("", text: $searchString, prompt: Text("Search \(filterType.lowercased())..."))
 			}.background(
 				.regularMaterial,
 				   in: Rectangle()
 			   )
-		   }).frame(width: 350).toolbar(content: {
-			   Button {
-				   showing = false
-			   } label: {
-				   Text("Cancel")
-			   }
-				if filterType == FilterType.saved.rawValue {
-					Button(role: .destructive) {
-						showingDeleteDialog = true
-					} label: {
-						Text("Delete")
-					}.disabled(buttonShouldBeDisabled())
-					Button {
-						showingRenameAlert = true
-					} label: {
-						Text("Rename")
-					}.disabled(buttonShouldBeDisabled())
-				}
-			   Button {
-				   if filterType == FilterType.presets.rawValue {
-					   asignPresetFilterComponentsToAppStorage()
-				   } else if filterType == FilterType.saved.rawValue {
-					   asignSavedFilterComponentsToAppStorage()
-				   }
-				   showing = false
-			   } label: {
-				   Text("Apply Filter")
-			   }.disabled(buttonShouldBeDisabled()).keyboardShortcut(.defaultAction)
-
-		   }).frame(width: 350, height: 615, alignment: .topLeading).padding().onChange(of: filterType) { _ in
+		   }).frame(width: 350).frame(width: 350, height: 615, alignment: .topLeading).padding().onChange(of: filterType) { _ in
 			   isEditing = false
 		   }
 			
@@ -197,75 +112,7 @@ struct FiltersView: View {
 						showing = false
 					}
 				} else if filterType == FilterType.saved.rawValue {
-					List(selection: $selectedSavedFilter) {
-						ForEach(savedFilters, id: \.self) { filter in
-							VStack(alignment: .center) {
-								HStack {
-									if isEditing {
-										VStack(spacing: 20) {
-											Button(role: .destructive) {
-												filterToDelete = filter
-												showingDeleteDialog = true
-											} label: {
-												Label("Delete", systemImage: "trash.fill").labelStyle(.titleOnly)
-											}.buttonStyle(.borderless).tint(Color.red)
-											Button {
-												filterToRename = filter
-												showingRenameAlert = true
-											} label: {
-												Label("Rename", systemImage: "pencil").labelStyle(.titleOnly)
-											}.buttonStyle(.borderless).tint(Color.indigo)
-										}.transition(.move(edge: .leading))
-									}
-									Spacer()
-									getFilteredImage(filter: filter).frame(width: isEditing ? 175 : 250, height: 175).transition(.scale).transition(.move(edge: .leading))
-									Spacer()
-								}
-								HStack {
-									Spacer()
-									Text(filter.name ?? "Saved Filter")
-									Spacer()
-								}
-							}.swipeActions(allowsFullSwipe: false) {
-								Button(role: .destructive) {
-									filterToDelete = filter
-									showingDeleteDialog = true
-							 } label: {
-								 Label("Delete", systemImage: "trash.fill").labelStyle(.iconOnly)
-							 }
-								Button {
-									filterToRename = filter
-									showingRenameAlert = true
-							 } label: {
-								 Label("Rename", systemImage: "pencil").labelStyle(.iconOnly)
-							 }.tint(.indigo)
-
-							}
-						}.onDelete(perform: delete)
-					}.listStyle(.sidebar).onChange(of: selectedSavedFilter) { newValue in
-						asignSavedFilterComponentsToAppStorage()
-						showing = false
-					}.confirmationDialog(Text("Are you sure you want to delete this filter?"), isPresented: $showingDeleteDialog) {
-						Button(role: .destructive) {
-							   DispatchQueue.main.async {
-								   if let filter = filterToDelete {
-									   managedObjectContext.delete(filter)
-									   selectedSavedFilter = nil
-									   do {
-										   try managedObjectContext.save()
-									   } catch {
-										   
-									   }
-								   }
-							   }
-						   } label: {
-							   Text("Delete Filter")
-						   }
-					   }.alert("Rename Your Filter", isPresented: $showingRenameAlert, actions: {
-						   RenameAlert(selectedSavedFilter: $filterToRename).environment(\.managedObjectContext, managedObjectContext)
-	 }, message: {
-		 Text("Eenter a new name for your filter:")
-	 })
+					SavedFiltersView(showing: $showing, searchString: searchString)
 				} else {
 					EmptyView()
 				}
@@ -281,64 +128,17 @@ struct FiltersView: View {
 						.regularMaterial,
 						in: Rectangle()
 					)
-					TextField("", text: .constant(""), prompt: Text("Search \(filterType.lowercased())...")).submitLabel(.done).font(.title3).padding(5).background(
+					TextField("", text: $searchString, prompt: Text("Search \(filterType.lowercased())...")).submitLabel(.search).font(.title3).padding(5).background(
 						   .regularMaterial,
 			in: Rectangle()
 				)
 				}
-			}).toolbar {
-				ToolbarItem(placement: .navigation, content: {
-					if filterType == FilterType.saved.rawValue {
-						Button {
-							if isEditing {
-								do {
-									try managedObjectContext.save()
-								} catch {
-									
-								}
-							}
-							withAnimation {
-								isEditing.toggle()
-							}
-						} label: {
-							Text(isEditing ? "Done" : "Edit")
-						}
-					}
-				})
-					ToolbarItem(placement: .primaryAction, content: {
-						Button {
-							showing = false
-						} label: {
-							Text("Cancel")
-						}
-					})
-				
-			}.navigationTitle(Text("Stored Filters")).navigationBarTitleDisplayMode(.inline)
+			}).navigationTitle(Text("Stored Filters")).navigationBarTitleDisplayMode(.inline)
 		}
 #endif
     }
 	
 	func getFilteredImage(filter: FilterModel) -> some View {
-		return getImage().resizable().aspectRatio(contentMode: .fit).if(filter.invertColors, transform: { view in
-			view.colorInvert()
-		}).if(filter.useHueRotation, transform: { view in
-			view.hueRotation(.degrees(filter.hueRotation))
-		}).if(filter.useContrast, transform: { view in
-			view.contrast(filter.contrast)
-		}).if(filter.useColorMultiply, transform: { view in
-			view.colorMultiply(Color(.sRGB, red: filter.colorMultiplyR, green: filter.colorMultiplyG, blue: filter.colorMultiplyB, opacity: filter.colorMultiplyO))
-		}).if(filter.useSaturation, transform: { view in
-				view.saturation(filter.saturation)
-			}).if(filter.useGrayscale, transform: { view in
-				view.grayscale(filter.grayscale)
-			}).if(filter.useOpacity, transform: { view in
-				view.opacity(filter.opacity)
-			}).if(filter.useBlur) { view in
-			view.blur(radius: filter.blur)
-		}
-	}
-	
-	func getFilteredImage(filter: Filter) -> some View {
 		return getImage().resizable().aspectRatio(contentMode: .fit).if(filter.invertColors, transform: { view in
 			view.colorInvert()
 		}).if(filter.useHueRotation, transform: { view in
@@ -390,46 +190,6 @@ struct FiltersView: View {
 		}
 	}
 	
-	func asignSavedFilterComponentsToAppStorage() {
-		if let filter = selectedSavedFilter {
-			invertColors = filter.invertColors
-			useHueRotation = filter.useHueRotation
-			hueRotation = filter.hueRotation
-			useContrast = filter.useContrast
-			contrast = filter.contrast
-			useColorMultiply = filter.useColorMultiply
-			colorMultiplyColor = Color(.sRGB, red: filter.colorMultiplyR, green: filter.colorMultiplyG, blue: filter.colorMultiplyB, opacity: filter.colorMultiplyO)
-			useSaturation = filter.useSaturation
-			saturation = filter.saturation
-			useGrayscale = filter.useGrayscale
-			grayscale = filter.grayscale
-			useOpacity = filter.useOpacity
-			opacity = filter.opacity
-			useBlur = filter.useBlur
-			blur = filter.blur
-		}
-	}
-	
-	func delete(filter: Filter) {
-			do {
-				managedObjectContext.delete(filter)
-				selectedSavedFilter = nil
-				try managedObjectContext.save()
-			} catch {
-				
-			}
-	}
-	
-	func delete(at offsets: IndexSet) {
-		for index in offsets {
-			do {
-				managedObjectContext.delete(savedFilters[index])
-				try managedObjectContext.save()
-			} catch {
-				
-			}
-		}
-	}
 	func buttonShouldBeDisabled() -> Bool {
 		return (filterType == FilterType.saved.rawValue && selectedSavedFilter == nil) || (filterType == FilterType.presets.rawValue && selectedPreset == nil)
 	}
---
+++ Filter Art/SavedFiltersView.swift
@@ -0,0 +1,307 @@
+//
+//  SavedFiltersView.swift
+//  Filter Art
+//
+//  Created by Matt Roberts on 1/27/23.
+//
+
+import SwiftUI
+
+struct SavedFiltersView: View {
+	@Environment(\.managedObjectContext) var managedObjectContext
+	@FetchRequest(sortDescriptors: [SortDescriptor(\.saveDate)]) var savedFilters: FetchedResults<Filter>
+	@AppStorage("imageInvertColors") private var invertColors: Bool = false
+	@AppStorage("imageHueRotation") private var hueRotation: Double = 0
+	@AppStorage("imageUseHueRotation") private var useHueRotation: Bool = false
+	@AppStorage("imageContrast") private var contrast: Double = 1
+	@AppStorage("imageUseContrast") private var useContrast: Bool = false
+	@AppStorage("imageUseColorMultiply") private var useColorMultiply: Bool = false
+	@AppStorage("imageColorMultiplyColor") private var colorMultiplyColor: Color = Color.blue
+	@AppStorage("imageUseSaturation") private var useSaturation: Bool = false
+	@AppStorage("imageSaturation") private var saturation: Double = 1
+	@AppStorage("imageUseGrayscale") private var useGrayscale: Bool = false
+	@AppStorage("imageGrayscale") private var grayscale: Double = 0
+	@AppStorage("imageUseOpacity") private var useOpacity: Bool = false
+	@AppStorage("imageOpacity") private var opacity: Double = 1
+	@AppStorage("imageUseBlur") private var useBlur: Bool = false
+	@AppStorage("imageBlur") private var blur: Double = 0
+	@AppStorage("imageUseOriginalImage") private var useOriginalImage: Bool = true
+	@EnvironmentObject var imageDataStore: ImageDataStore
+	@Binding var showing: Bool
+	@State var selectedSavedFilter: Filter? = nil
+	@State var isEditing = false
+	@State var showingDeleteDialog = false
+	@State var showingRenameAlert = false
+	@State var filterToDelete: Filter? = nil
+	@State var filterToRename: Filter? = nil
+	
+	init(showing: Binding<Bool>, searchString: String) {
+		_showing = showing
+		if searchString == "" {
+			_savedFilters = FetchRequest<Filter>(sortDescriptors: [SortDescriptor(\.saveDate)])
+		} else {
+			_savedFilters = FetchRequest<Filter>(sortDescriptors: [SortDescriptor(\.saveDate)], predicate: NSPredicate(format: "name CONTAINS[c] %@", searchString))
+		}
+	}
+	
+	var body: some View {
+		#if os(macOS)
+		VStack(alignment: .center, content: {
+			List(selection: $selectedSavedFilter) {
+				ForEach(savedFilters, id: \.self) { filter in
+					VStack(alignment: .center) {
+						HStack {
+							Spacer()
+							if isEditing {
+								VStack {
+									Button(role: .destructive) {
+										filterToDelete = filter
+										showingDeleteDialog = true
+									} label: {
+										Label("Delete", systemImage: "trash.fill").labelStyle(.titleOnly)
+									}.buttonStyle(.borderless).tint(Color.red)
+									Button {
+										filterToRename = filter
+										showingRenameAlert = true
+									} label: {
+										Label("Rename", systemImage: "pencil").labelStyle(.titleOnly)
+									}.buttonStyle(.borderless).tint(Color.indigo)
+								}.transition(.move(edge: .leading))
+								
+								Spacer()
+							}
+							getFilteredImage(filter: filter).frame(width: 250, height: 175).transition(.move(edge: .leading))
+							Spacer()
+						}
+						HStack {
+							Spacer()
+							Text(filter.name ?? "Saved Filter")
+							Spacer()
+						}
+					}
+				}
+			}.listStyle(.sidebar).confirmationDialog(Text("Are you sure you want to delete this filter?"), isPresented: $showingDeleteDialog) {
+									  Button(role: .destructive) {
+										  DispatchQueue.main.async {
+											  if let filter = selectedSavedFilter {
+												  managedObjectContext.delete(filter)
+												  selectedSavedFilter = nil
+												  do {
+													  try managedObjectContext.save()
+												  } catch {
+													  
+												  }
+											  }
+										  }
+									  } label: {
+										  Text("Delete Filter")
+									  }
+									  
+								  }.alert("Rename Your Filter", isPresented: $showingRenameAlert, actions: {
+									  RenameAlert(selectedSavedFilter: $selectedSavedFilter).environment(\.managedObjectContext, managedObjectContext)
+								  }, message: {
+									  Text("Eenter a new name for your filter:")
+								  })
+		}).toolbar(content: {
+			Button {
+				showing = false
+			} label: {
+				Text("Cancel")
+			}
+				 Button(role: .destructive) {
+					 showingDeleteDialog = true
+				 } label: {
+					 Text("Delete")
+				 }.disabled(selectedSavedFilter == nil)
+				 Button {
+					 showingRenameAlert = true
+				 } label: {
+					 Text("Rename")
+				 }.disabled(selectedSavedFilter == nil)
+			Button {
+					asignSavedFilterComponentsToAppStorage()
+				showing = false
+			} label: {
+				Text("Apply Filter")
+			}.disabled(selectedSavedFilter == nil).keyboardShortcut(.defaultAction)
+
+		})
+		#else
+		List(selection: $selectedSavedFilter) {
+			ForEach(savedFilters, id: \.self) { filter in
+				VStack(alignment: .center) {
+					HStack {
+						if isEditing {
+							VStack(spacing: 20) {
+								Button(role: .destructive) {
+									filterToDelete = filter
+									showingDeleteDialog = true
+								} label: {
+									Label("Delete", systemImage: "trash.fill").labelStyle(.titleOnly)
+								}.buttonStyle(.borderless).tint(Color.red)
+								Button {
+									filterToRename = filter
+									showingRenameAlert = true
+								} label: {
+									Label("Rename", systemImage: "pencil").labelStyle(.titleOnly)
+								}.buttonStyle(.borderless).tint(Color.indigo)
+							}.transition(.move(edge: .leading))
+						}
+						Spacer()
+						getFilteredImage(filter: filter).frame(width: isEditing ? 175 : 250, height: 175).transition(.scale).transition(.move(edge: .leading))
+						Spacer()
+					}
+					HStack {
+						Spacer()
+						Text(filter.name ?? "Saved Filter")
+						Spacer()
+					}
+				}.swipeActions(allowsFullSwipe: false) {
+					Button(role: .destructive) {
+						filterToDelete = filter
+						showingDeleteDialog = true
+				 } label: {
+					 Label("Delete", systemImage: "trash.fill").labelStyle(.iconOnly)
+				 }
+					Button {
+						filterToRename = filter
+						showingRenameAlert = true
+				 } label: {
+					 Label("Rename", systemImage: "pencil").labelStyle(.iconOnly)
+				 }.tint(.indigo)
+
+				}
+			}.onDelete(perform: delete)
+		}.listStyle(.sidebar).onChange(of: selectedSavedFilter) { newValue in
+			asignSavedFilterComponentsToAppStorage()
+			showing = false
+		}.confirmationDialog(Text("Are you sure you want to delete this filter?"), isPresented: $showingDeleteDialog) {
+			Button(role: .destructive) {
+				   DispatchQueue.main.async {
+					   if let filter = filterToDelete {
+						   managedObjectContext.delete(filter)
+						   selectedSavedFilter = nil
+						   do {
+							   try managedObjectContext.save()
+						   } catch {
+							   
+						   }
+					   }
+				   }
+			   } label: {
+				   Text("Delete Filter")
+			   }
+		   }.alert("Rename Your Filter", isPresented: $showingRenameAlert, actions: {
+			   RenameAlert(selectedSavedFilter: $filterToRename).environment(\.managedObjectContext, managedObjectContext)
+}, message: {
+Text("Eenter a new name for your filter:")
+}).toolbar {
+	ToolbarItem(placement: .navigation, content: {
+					 Button {
+						 if isEditing {
+							 do {
+								 try managedObjectContext.save()
+							 } catch {
+								 
+							 }
+						 }
+						 withAnimation {
+							 isEditing.toggle()
+						 }
+					 } label: {
+						 Text(isEditing ? "Done" : "Edit")
+					 }
+			 })
+				 ToolbarItem(placement: .primaryAction, content: {
+					 Button {
+						 showing = false
+					 } label: {
+						 Text("Cancel")
+					 }
+				 })
+			 
+		 }
+		#endif
+	}
+	
+	func getFilteredImage(filter: Filter) -> some View {
+		return getImage().resizable().aspectRatio(contentMode: .fit).if(filter.invertColors, transform: { view in
+			view.colorInvert()
+		}).if(filter.useHueRotation, transform: { view in
+			view.hueRotation(.degrees(filter.hueRotation))
+		}).if(filter.useContrast, transform: { view in
+			view.contrast(filter.contrast)
+		}).if(filter.useColorMultiply, transform: { view in
+			view.colorMultiply(Color(.sRGB, red: filter.colorMultiplyR, green: filter.colorMultiplyG, blue: filter.colorMultiplyB, opacity: filter.colorMultiplyO))
+		}).if(filter.useSaturation, transform: { view in
+				view.saturation(filter.saturation)
+			}).if(filter.useGrayscale, transform: { view in
+				view.grayscale(filter.grayscale)
+			}).if(filter.useOpacity, transform: { view in
+				view.opacity(filter.opacity)
+			}).if(filter.useBlur) { view in
+			view.blur(radius: filter.blur)
+		}
+	}
+	
+	func getImage() -> Image {
+		if useOriginalImage {
+			return Image("FallColors")
+		} else {
+#if os(macOS)
+			return Image(nsImage: (NSImage(data: imageDataStore.imageData) ?? NSImage()))
+#else
+			return Image(uiImage: (UIImage(data: imageDataStore.imageData)  ?? UIImage()))
+#endif
+		}
+	}
+	
+	func asignSavedFilterComponentsToAppStorage() {
+		if let filter = selectedSavedFilter {
+			invertColors = filter.invertColors
+			useHueRotation = filter.useHueRotation
+			hueRotation = filter.hueRotation
+			useContrast = filter.useContrast
+			contrast = filter.contrast
+			useColorMultiply = filter.useColorMultiply
+			colorMultiplyColor = Color(.sRGB, red: filter.colorMultiplyR, green: filter.colorMultiplyG, blue: filter.colorMultiplyB, opacity: filter.colorMultiplyO)
+			useSaturation = filter.useSaturation
+			saturation = filter.saturation
+			useGrayscale = filter.useGrayscale
+			grayscale = filter.grayscale
+			useOpacity = filter.useOpacity
+			opacity = filter.opacity
+			useBlur = filter.useBlur
+			blur = filter.blur
+		}
+	}
+	
+	func delete(filter: Filter) {
+			do {
+				managedObjectContext.delete(filter)
+				selectedSavedFilter = nil
+				try managedObjectContext.save()
+			} catch {
+				
+			}
+	}
+	
+	func delete(at offsets: IndexSet) {
+		for index in offsets {
+			do {
+				managedObjectContext.delete(savedFilters[index])
+				try managedObjectContext.save()
+			} catch {
+				
+			}
+		}
+	}
+}
+
+/*
+struct SavedFiltersView_Previews: PreviewProvider {
+	static var previews: some View {
+		SavedFiltersView()
+	}
+}
+*/
